7a823d2d90f4e450fc82ed8e0f81fa777483fd7f
rlahteen <lahteri@gmail.com>
2024-11-09 22:18:38 +0000

    instuctions after login

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
     setNotes([]);
   };
 
-  const addNote = (text, height, color) => {
-    const newNote = { id: Date.now(), text, height, color, x: 0, y: 0 };
+  const addNote = (text, height) => {
+    const newNote = { id: Date.now(), text, height, x: 0, y: 0 };
     setNotes([...notes, newNote]);
   };
 
@@ -36,11 +36,12 @@ function App() {
 
   return (
     <div style={{ display: "flex" }}>
-      <InstructionsPanel />
+      
       <div style={{ marginLeft: "20vw", padding: "20px", width: "80vw", textAlign: "center" }}>
         {isAuthenticated ? (
           <>
             <h1>Welcome, {username}!</h1>
+            <InstructionsPanel />
             <button onClick={handleLogout}>Logout</button>
             <Notes
               notes={notes}
