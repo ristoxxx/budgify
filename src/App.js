0fd3e95538c6f02514330af662f9cef922ff4662
rlahteen <lahteri@gmail.com>
2024-11-09 20:49:17 +0000

    Update start page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import logo from './LogoBudgify.svg';
 import './App.css';
 import Login from "./Login";
+import React, { useState } from "react";
 
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      
+  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [username, setUsername] = useState("");
 
-      </header>
+  const handleLogin = (username) => {
+    setIsAuthenticated(true);
+    setUsername(username);
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    setUsername("");
+  };
+
+  return (
+      <div className="App">
+
+      {isAuthenticated ? (
+        <>
+          <h1>Welcome, {username}!</h1>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      ) : (
+        <Login onLogin={handleLogin} />
+      )}
     </div>
   );
 }
